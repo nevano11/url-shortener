a1ed9d3a24894ecc1a0aa75562e7a552b402bc19
vash windows <o_otter@bk.ru>
2023-09-08 19:19:12 +0300

    Create base to developing api

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,58 @@
-package cmd
+package main
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
 
 func main() {
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.Info("Starting url-shortener app")
 
+	// Read config
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("Failed to read config: %s", err.Error())
+	}
+
+	// Configure logger
+	if err := configureLogger(viper.GetString("logger.log-level")); err != nil {
+		logrus.Fatalf("Failed to configure logger: %s", err.Error())
+	}
+
+	// Create context
+	ctx := context.Background()
+
+	// Database connection
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     ":" + viper.GetString("redis.addr"),
+		Password: viper.GetString("redis.password"),
+		DB:       viper.GetInt("redis.entryTime"),
+	})
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		logrus.Fatalf("Failed to ping database: %s", err.Error())
+	}
+
+	// Repository, server, handler
+
+	// Routes
+
+	// Server
+}
+
+// Configuration
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
+
+func configureLogger(logLevel string) error {
+	lvl, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		return err
+	}
+	logrus.SetLevel(lvl)
+	return nil
 }
